f09977038e9ff88fc9d349cb387d6fce7646930c
jrh <joshuaharris1103@gmail.com>
2023-02-07 08:31:57 -0500

    Burger stacker with classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import BurgerStacker from './BurgerStacker'
 
 function App() {
   return (
-    <>
-    </>
-  );
+    <div className="App">
+      <BurgerStacker/>
+    </div>
+  )
 }
 
-export default App;
+export default App
