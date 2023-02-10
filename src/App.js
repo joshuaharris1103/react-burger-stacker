28cccb861b073d1383884e9db5b27ae37f3c2cc9
jrh <joshuaharris1103@gmail.com>
2023-02-09 21:22:51 -0500

    Fixed branch direction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
+import React from 'react'
 import BurgerStacker from './BurgerStacker'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <BurgerStacker/>
--- src/BurgerStacker.js
+++ src/BurgerStacker.js
@@ -2,26 +2,24 @@ import React, { Component } from 'react'
 import IngredientList from './IngredientList'
 import BurgerPane from './BurgerPane'
 
-export default class BurgerStacker extends Component {
-    state = {
-        ingredients: [
-            {name: 'Kaiser Bun', color: 'saddlebrown'},
-            {name: 'Sesame Bun', color: 'sandybrown'},
-            {name: 'Gluten Free Bun', color: 'peru'},
-            {name: 'Lettuce Wrap', color: 'olivedrab'},
-            {name: 'Beef Patty', color: '#3F250B'},
-            {name: 'Soy Patty', color: '#3F250B'},
-            {name: 'Black Bean Patty', color: '#3F250B'},
-            {name: 'Chicken Patty', color: 'burlywood'},
-            {name: 'Lettuce', color: 'lawngreen'},
-            {name: 'Tomato', color: 'tomato'},
-            {name: 'Bacon', color: 'maroon'},
-            {name: 'Onion', color: 'lightyellow'},
-            {name: 'Cheese', color: 'gold'}
-        ],
-        burgerIngredients: []
-    }
+const ingredientsArray = [
+    {name: 'Kaiser Bun', color: 'saddlebrown'},
+    {name: 'Sesame Bun', color: 'sandybrown'},
+    {name: 'Gluten Free Bun', color: 'peru'},
+    {name: 'Lettuce Wrap', color: 'olivedrab'},
+    {name: 'Beef Patty', color: '#3F250B'},
+    {name: 'Soy Patty', color: '#3F250B'},
+    {name: 'Black Bean Patty', color: '#3F250B'},
+    {name: 'Chicken Patty', color: 'burlywood'},
+    {name: 'Lettuce', color: 'lawngreen'},
+    {name: 'Tomato', color: 'tomato'},
+    {name: 'Bacon', color: 'maroon'},
+    {name: 'Onion', color: 'lightyellow'},
+    {name: 'Cheese', color: 'gold'}
+  ]
 
+    const [ingredients, setIngredients] = useState(ingredientsArray)
+    console.log(ingredients)
     
     addToStack = (e) => {
         const ingName = e.target.innerText
@@ -61,7 +59,7 @@ export default class BurgerStacker extends Component {
         })
     }
 
-    render () {
+    const render = () => {
         return (
             <div>
                 <h1>Burger Stacker</h1>
@@ -79,4 +77,3 @@ export default class BurgerStacker extends Component {
             </div>
         )
     }
-}
